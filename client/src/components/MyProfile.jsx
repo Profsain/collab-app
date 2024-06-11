e4e992df24c5f613d0fdc4c1393d713e2d4e7640
profsain <husseinimudiking@gmail.com>
2024-06-11 17:52:44 +0100

    add my profile

---
+++ client/src/components/MyProfile.jsx
@@ -0,0 +1,24 @@
+
+
+const MyProfile = () => {
+  return (
+    <div>
+      <img
+        src="https://media.licdn.com/dms/image/D4D22AQHw6PgQgjoupg/feedshare-shrink_800/0/1689754282921?e=2147483647&v=beta&t=p4GPb5LjNpyDH6qyRGdkKC36mfkeRVlb4bwmmfz0ofU"
+        alt="profsain"
+      />
+
+      <div>
+        <h1>Profsain Husain</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          quaerat, ipsum repellat eligendi quis deleniti soluta ipsa ad numquam
+          perferendis beatae porro. Eos molestiae provident corporis voluptatum
+          minima, excepturi et?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
