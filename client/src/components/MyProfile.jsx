381791ade952e20e5946cbeb2c7554f45ed47a9b
martyns <itumo147@yahoo.com>
2024-06-11 17:55:14 +0100

    myprofile

---
+++ client/src/components/MyProfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Myprofle = () => {
+  return (
+    <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUbUTu9KOD-EBAA3IiBLCPl3ZeyP0Rmb954g&s" alt="martyns" />
+
+        <div>
+            my name is martyns
+        </div>
+    </div>
+
+  )
+}
+
+export default Myprofle
