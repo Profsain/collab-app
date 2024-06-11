0f3d194d475936cc37438bd8762858ffc7f1e45b
TettehSolomon <tettehsolomon106@gmail.com>
2024-06-11 17:48:42 +0100

    add profile component

---
+++ client/src/component/MyProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MyProfile = () => {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoRGyXmHy_6aIgXYqWHdOT3KjfmnuSyxypw&s" alt="background" />  
+      <div>
+        <h1>Background Image</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laboriosam nesciunt reprehenderit eos sit nulla consequuntur necessitatibus unde officiis dicta amet, nam velit quis impedit explicabo excepturi in voluptates ab accusamus possimus et fugit voluptatem. Architecto, tempore? Illo, esse fugit!</p>
+      </div>
+    </div>
+  )
+}
+
+export default MyProfile
